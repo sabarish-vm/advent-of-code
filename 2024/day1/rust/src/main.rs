ab61523139e264f9febc9148976eda866198a2f6
sabarish-vm <sabarish.science@gmail.com>
2024-12-29 00:39:04 +0100

    removed dependency on ndarray

--- 2024/day1/rust/src/main.rs
+++ 2024/day1/rust/src/main.rs
@@ -1,19 +1,19 @@
 use core::panic;
-use ndarray::Array;
 
-fn problem1(matrix: &Vec<Vec<f64>>) {
+fn problem1(matrix: &[Vec<f64>]) {
     let mut col0: Vec<f64> = matrix.iter().map(|row| row[0]).collect();
     let mut col1: Vec<f64> = matrix.iter().map(|row| row[1]).collect();
     col0.sort_by(|a: &f64, b| a.partial_cmp(b).unwrap());
     col1.sort_by(|a: &f64, b| a.partial_cmp(b).unwrap());
-    let array0 = Array::from_vec(col0);
-    let array1 = Array::from_vec(col1);
-    let diff = array1 - array0;
-    let ans = diff.abs().sum();
+    let mut diff: Vec<f64> = Vec::new();
+    for i in 0..col0.len() {
+        diff.push((col0[i] - col1[i]).abs());
+    }
+    let ans: f64 = diff.iter().sum();
     println!("Part1 = {}", ans);
 }
 
-fn problem2(matrix: &Vec<Vec<f64>>) {
+fn problem2(matrix: &[Vec<f64>]) {
     let mut counts: Vec<f64> = Vec::new();
     for irow in 0..matrix.len() {
         let val1 = matrix[irow][0];
@@ -26,7 +26,7 @@ fn problem2(matrix: &Vec<Vec<f64>>) {
 
 fn main() {
     let contents = match std::fs::read_to_string(
-        "/Users/sabarish/Documents/Computing/advent24/day1/input.txt",
+        "/Users/sabarish/Documents/Computing/advent-of-code/2024/day1/input.txt",
     ) {
         Ok(x) => x,
         Err(x) => panic!("Error reading file {x}"),
